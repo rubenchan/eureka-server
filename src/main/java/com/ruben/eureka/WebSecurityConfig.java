1cde7a351fc75b9b9c21b4882d7e126eecfee02b
ruben <rubenchan@live.com>
2019-04-02 11:03:42 +0800

    commit

---
+++ src/main/java/com/ruben/eureka/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ruben.eureka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable(); //关闭CSRF攻击防御，如不关闭，Eureka Client则会找不到Eureka Server而报异常。
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+
+    }
+
+}
